cd62de46181da9f7b1126a391d01bc7b3ca6845b
Eli Sacks <bastlifa@gmail.com>
2020-01-17 14:59:22 -0500

    cleaned up comments

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -13,29 +13,6 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
     a = []
-    # for i in range(len(weights)):
-    #     if hash_table_retrieve(ht, weights[i]):
-    #         b = hash_table_retrieve(ht, weights[i])
-    #         c = []
-    #         c.append(b)
-    #         c.append(i)
-    #         hash_table_insert(ht, weights[i], c)
-    #     else:
-    #         hash_table_insert(ht, weights[i], i)
-
-    # for i in weights:
-    #     if hash_table_retrieve(ht, limit - i):
-    #         try:
-    #             # len(hash_table_retrieve(ht, limit - i))
-    #             return f'aadfasdf, {hash_table_retrieve(ht, limit - i)}'
-    #             # b = hash_table_retrieve(ht, limit - i)
-    #             # a.append(b[:len(b) - 1])
-    #             # a.append(b[:len(b) - 2])
-    #         except:
-    #             a.append(hash_table_retrieve(ht, limit - i))
-    #             # hash_table_remove(ht, limit - i)
-    #             a.append(hash_table_retrieve(ht, i))
-    #             return a
 
     for i in range(len(weights)):
         if hash_table_retrieve(ht, weights[i]) is not None:
@@ -60,8 +37,3 @@ def print_answer(answer):
     else:
         print("None")
 
-# weights_2 = [4, 4]
-# answer_2 = get_indices_of_item_weights(weights_2, 2, 8)
-# # for i in range(len(answer_2)):
-# #     answer_2[i] = str(answer_2[i])
-# print_answer(answer_2)
